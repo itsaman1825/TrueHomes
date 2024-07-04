cdf6ef5ccb6dcffd8b3695f347417f917cbdee72
yuvraj <jindalyuvraj2@gmail.com>
2024-07-04 14:23:17 +0530

    fixing error

--- api/index.js
+++ api/index.js
@@ -22,8 +22,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-app.listen(11891,() => {
-        console.log('Server is running at port 3000!!')
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,() => {
+        console.log(`Server is running at port ${PORT}`)
     }
 );
 
